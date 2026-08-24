52d5b24003bda6a7738eb055ab89095a7d57f0e1
Jacob Lockett <jacobwyattlockett@gmail.com>
2026-08-23 23:52:38 -0400

    Wire Map v2 Rust runtime entrypoint

---
+++ skills/map/src/main.rs
@@ -0,0 +1,9 @@
+include!("parts/00.rs");
+include!("parts/01.rs");
+include!("parts/02.rs");
+include!("parts/03.rs");
+include!("parts/04.rs");
+include!("parts/05.rs");
+include!("parts/06.rs");
+include!("parts/07.rs");
+include!("parts/08.rs");
